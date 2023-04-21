b27a9bbf6cd84a221bc70405f817e4ff27cccb86
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 14:50:46 -0300

    Constantes - Regras, e concatenações

---
+++ aula4/constantes.js
@@ -0,0 +1,24 @@
+
+// Não podemos criar variáveis com palabras reservadas
+// Variaveis precisam ter nomes que descrevam a mesma
+// Não pode coemçar o nome de uma variável com um número
+// Nao pode conter espaços ou traços
+// Utilizamos camelCase
+// Case sensitive
+// NAO UTILIZAR VAR, UTILIZAR CONST
+// A constante deve ter valor na sua inicialização
+// Não pode modificar o valor
+
+const nome = "Moises Ferreira";
+// nome = "Moises Ferreira de Sousa"
+console.log(nome);
+
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero*segundoNumero
+const resultadoDuplicado = resultado * 2;
+console.log(resultado)
+console.log(resultadoDuplicado )
+
+console.log(typeof(nome));
+console.log(typeof(primeiroNumero + segundoNumero));
