abcfd91323de1030e180b26b3fa46ae286d6c619
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 20:36:31 -0300

    Objeto math, e alguns métodos importantes

---
+++ aula13/objetoMath.js
@@ -0,0 +1,12 @@
+let num1 = 9.54578;
+console.log(Math.floor(num1))
+console.log(Math.ceil(num1))
+console.log(Math.round(num1))
+console.log(Math.max(1,2,3,4,5,-10, 423, 4342, 221,2124))
+console.log(Math.min(1,2,3,4,5,-10, 423, 4342, 221,2124))
+console.log(Math.random())
+console.log(Math.PI)
+// Calcular a raiz quadrada
+console.log(16 ** 0.5);
+// Divisões por 0 em JavaScript retorna Infinity
+console.log(100/0);
