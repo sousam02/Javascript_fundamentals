db269c59c3069b689405db7be2f4332a1dc6a589
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 13:25:26 -0300

    Commit de primeiras aulas

---
+++ aula1/index.js
@@ -0,0 +1,9 @@
+console.log("Moises Ferreira 'de' Sousa"); //String (Gerado automaticamente a tipagem)
+console.log('Moises "Ferreira" de Sousa')
+console.log(`Moises "Ferreira" 'de' Sousa`)
+
+console.log(123456, 123.4567)
+
+console.log(35, 15,85, "Moises Ferreira de Sousa")
+
+console.log('Meu nome é "Moises Ferreira de Sousa". Estou aprendendo JavaScript às', 10, "da manhã")
