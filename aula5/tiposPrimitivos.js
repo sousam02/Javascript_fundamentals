45941872b1669b9c82aaa4ac9f5fbcfb42245587
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 15:42:59 -0300

    Tipos primitivos: Quais são e características

---
+++ aula5/tiposPrimitivos.js
@@ -0,0 +1,7 @@
+// String, number, undefined, null, boolean, symbol
+const nome="Luiz"; // string
+const num = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; // Undefined -> Não aponta pra local nenhum na memória
+const sobrenomeAluno = null; //Nulo -> nao aponta pra local nenhum na memória
+const aprovado = false; // Boolean -> Verdadeiro ou falso
