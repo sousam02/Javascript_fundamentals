2707fe6c1ad2208dd6172b163a71040d11c7afa7
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 16:25:59 -0300

    Alert, Confirm e prompt, e como manipular seus retornos

---
+++ aula11/js/script.js
@@ -0,0 +1,3 @@
+window.alert("mensagem no alert.")
+let confirma = window.confirm('Deseja realmente apagar?') //Retorna um Boolean
+let nome = window.prompt('digite o seu nome'); //Retorna o que o usuário escrever no prompt, sempre em String
