608f9faa84fd8f53086ff2adab0e90bb5c920fe2
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 14:32:38 -0300

    Variaveis em JS, regras e etc

---
+++ aula4/variaveis.js
@@ -0,0 +1,20 @@
+let nome = "Felipe";
+
+console.log(nome, "nasceu em 1984");
+console.log("Em 2000", nome, "conheceu Maria");
+console.log(nome,"casou-se com Maria em 2012");
+console.log("Maria teve um filho com",nome, "em 2015");
+console.log("O filho de", nome,"se chama Eduardo");
+
+// Não podemos criar variáveis com palabras reservadas
+// Variaveis precisam ter nomes que descrevam a mesma
+// Não pode coemçar o nome de uma variável com um número
+// Nao pode conter espaços ou traços
+// Utilizamos camelCase
+// Case sensitive
+
+let nomeCompleto = "Moises Ferreira de Sousa";
+let nomecompleto = "Moises Sousa Ferreira";
+
+console.log(nomeCompleto);
+console.log(nomecompleto);
