29851107382bbbf2cea75d98867d65939e58557a
Moises Sousa <moises.sousa583@gmail.com>
2023-04-21 16:01:35 -0300

    Operadores aritméticos

---
+++ aula6/operadores.js
@@ -0,0 +1,23 @@
+/*
+*Aritméticos
+*   + Adição / Concatenação
+*   - Subtração
+*   * Multiplicação
+*   ** Potenciação
+*   % Resto da divisão
+* 
+*/
+const num1 = 5;
+const num2 = 10;
+
+const soma = num1 + num2;
+const subtracao = num1 - num2;
+const multiplicacao = num1 * num2;
+const divisao = num1 / num2;
+const restoDivisao = num1 % num2;
+const potenciacao = num2 ** num1;
+
+const str1 = "moises";
+
+// Operação que gera erro
+// const contaErro = num1 * str1;
