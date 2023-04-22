a1e6adc38108d100e1b27b83555539cef8a61bc7
Moises Ferreira de Sousa <60903024+sousam02@users.noreply.github.com>
2023-04-22 15:47:00 -0300

    Arrays e métodos para manipulação de arrays

---
+++ javascript-basico/arrays/index.js
@@ -0,0 +1,22 @@
+const alunos = ['luiz', 'Maria', 'João']
+
+
+alunos.push("luiza")
+alunos.push("eduardo")
+
+// console.log(alunos[50]) // É possivel acessar um indice que nao existe, e o retorno é undefined
+
+// delete alunos[1] // Remove o elemento, mas nao reordena o array
+
+// const removido = alunos.pop() // Remove o último elemento do array
+// const removido2 = alunos.shift() // Remove o primeiro elemento do array
+// console.log(removido)
+
+// alunos[0] = "eduardo";
+// alunos.unshift = "Luiza" //Adiciona um elemento no inicio do array
+
+// alunos.push("Moises", "Felipe") // Adiciona elementos no fim do array
+
+console.log(alunos)
+
+// console.log(alunos[1])
