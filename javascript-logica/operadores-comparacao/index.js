f6715de3183b31995994c53cf4b6342a043a697e
Moises Ferreira de Sousa <60903024+sousam02@users.noreply.github.com>
2023-04-29 11:27:07 -0300

    Operadores de comparação: quais são e exemplos

---
+++ javascript-logica/operadores-comparacao/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+
+> Maior que
+>=Maior ou igual a
+< Menor que
+<=Menor ou igual a
+== igualdade (Valor)
+===igualdade estrita, idêntica (Valor e tipo)
+!= diferente (Valor)
+!== diferença estrita (Valor e tipo)
+*/
+
+const num1 = 10
+const num2 = '10'
+
+const comp = num1 === num2;
+
+console.log(comp)
