35545384a1e18af4e2a134cd5cbf76e0120c87bd
Moises Ferreira de Sousa <60903024+sousam02@users.noreply.github.com>
2023-05-02 21:31:40 -0300

    Síntaxe switch case e exemplo

---
+++ javascript-logica/switch-case/index.js
@@ -0,0 +1,30 @@
+const diaSemana = 3;
+let diaSemanaTexto;
+
+switch(diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+    default:
+        diaSemanaTexto = '';
+        break;
+}
+console.log(diaSemana, diaSemanaTexto)
