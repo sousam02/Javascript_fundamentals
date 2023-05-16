30723b891a84c0d64c9c6778fa48f58293f1f075
Moises Ferreira de Sousa <60903024+sousam02@users.noreply.github.com>
2023-05-15 23:32:11 -0300

    exercício utilizando ternários, e  arrow functions

---
+++ javascript-logica/exercicise-1/index.js
@@ -0,0 +1,17 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function max(x,y){
+    return x > y ? x : y;
+}
+
+const max2 = (x, y) => {
+    return x > y ? x : y;
+}
+
+const max3 = (x,y) => x > y ? x : y;
+
+console.log(max(1,2));
+console.log(max2(1,2));
+console.log(max3(1,2));
+
+
